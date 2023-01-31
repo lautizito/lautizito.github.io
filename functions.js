35476628e947691603c6415403f43f32b6ab24d8
lautizito <122745383+lautizito@users.noreply.github.com>
2023-01-31 17:23:35 -0300

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,4 @@
+document.getElementById('boton').onclick = function () {
+    console.log("Capturamos el click")
+    document.getElementById("demo").innerHTML = "Hola! Aprecio mucho que te hayas tomado el tiempo por leer mi Curriculum Vitae y espero que consideres mis habilidades, conocimientos y ganas para poder trabajar juntos. Gracias nuevamente por leerlo y espero hablar contigo pronto!"
+}
